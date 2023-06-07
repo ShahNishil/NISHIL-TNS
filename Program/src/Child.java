1798698f516617d874ec561174b544eb5e74d0e5
ShahNishil <shahnishil937@gmail.com>
2023-06-08 00:21:00 +0530

    commit

---
+++ Program/src/Child.java
@@ -0,0 +1,10 @@
+import Multiply.*;
+
+public class Child {
+
+	public static void main(String[] args) {
+		Multiplication obj=new Multiplication();
+		obj.multiply(12, 11);
+	}
+
+}
---
+++ Program/src/Multiply/Multiplication.java
@@ -0,0 +1,10 @@
+package Multiply;
+
+public class Multiplication {
+
+	public void multiply(int x,int y) {
+		System.out.println("Answer: "+ (x*y));
+	}
+	
+}
+  
