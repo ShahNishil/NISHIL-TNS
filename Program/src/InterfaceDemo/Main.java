0eed7d90566a86f0ef4bcfbaacfa9b7519e8e7bc
ShahNishil <shahnishil937@gmail.com>
2023-06-08 22:53:57 +0530

    commit

--- Program/src/InterfaceDemo/Main.java
+++ Program/src/InterfaceDemo/Main.java
@@ -2,12 +2,9 @@ package InterfaceDemo;
 
 public class Main {
     public static void main(String[] args) {
-        //length=10,width=12
-		Rectangle rectangle = new Rectangle(10, 12);
-		//radius=3
-        Circle circle = new Circle(3);
-        //base=4,height=6
-		Triangle triangle = new Triangle(4, 6);
+        Rectangle rectangle = new Rectangle(10, 12);
+		Circle circle = new Circle(3);
+        Triangle triangle = new Triangle(4, 6);
 
         System.out.println("Area of the Rectangle: " + rectangle.getArea());
         System.out.println("Area of the Circle: " + circle.getArea());
